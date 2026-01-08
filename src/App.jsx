f8c2929acda849d74cd07d416f48ce58eb86c710
programmeraza <karimberdiyevaziz077@gmail.com>
2026-01-08 19:35:43 +0500

    feat: final project v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.scss'
 import Code from './components/Code/Code'
 import Explore from './components/Explore/Explore'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 
@@ -12,6 +13,7 @@ function App() {
       <Hero/>
       <Explore/>
       <Code/>
+      <Footer/>
     </>
   )
 }
